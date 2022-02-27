663ba9fe8e7194f0df9f2e4f71f468b6a858bbfa
FerrumTheElement <74129774+FerrumTheElement@users.noreply.github.com>
2022-02-27 21:41:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import requests
 import commandfile
 from discord.ext import commands
 
-API_KEY = "YOUR_API"
-c = requests.get(f"https://api.hypixel.net/skyblock/bazaar?key={API_KEY}").json()
-#Requests stuff from the hypixel api when the variable "c" is used
 client = commands.Bot(command_prefix = '!b ')
     
 @client.event
