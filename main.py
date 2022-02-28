d1944b466c13726ca2bec73f2d9600d4855ac2bd
FerrumTheElement <74129774+FerrumTheElement@users.noreply.github.com>
2022-02-28 13:35:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 import commandfile
 from discord.ext import commands
 
@@ -9,7 +8,8 @@ async def on_ready():
     print("Ready!")
     
 @client.command()
-async def search(ctx,text):
-  await commandfile.search(ctx,text)
+async def search(ctx,*text):
+  await commandfile.search(ctx,*text)
 #Uses the file "commandfile.py" to search the prices of bazzar in the hypixel api & to print out the output.
+
 client.run('token',bot=True, reconnect=True)
